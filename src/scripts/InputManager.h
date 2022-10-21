90c658550cc4356c2d883e74eba6850cdf4cef67
Jose Ignacio Ferrer Vera <joseferrertec@gmail.com>
2022-10-20 20:51:15 -0500

    extra progress on tank header and input manager

--- src/scripts/InputManager.h
+++ src/scripts/InputManager.h
@@ -2,12 +2,24 @@
 #define INPUT_MANAGER_H
 
 #include <Godot.hpp>
+#include <Node.hpp>
 #include <Input.hpp>
 
 namespace godot{
 
+class InputManager : public Node {
 
+private:
+    
+public:
+    static void _register_methods();
 
+    InputManager();
+    ~InputManager();
+
+    
+
+};
 }
 
 #endif
--- src/scripts/Tank.h
+++ src/scripts/Tank.h
@@ -7,17 +7,37 @@
 #include <Spatial.hpp>
 #include <Ref.hpp>
 #include <PackedScene.hpp>
-#include <Input.hpp>
+#include <Vector3.hpp>
+#include "./InputManager.h"
 
 namespace godot {
 
-class Tank : public KinematicBody{
+class Tank : public KinematicBody {
     GODOT_CLASS(Tank, KinematicBody)
 
 private:
     MeshInstance* _head;
     Spatial* _cannon;
     Ref<PackedScene>* _bullet;
+    InputManager* _input_manager;
+
+    float _move_speed;
+    float _turn_speed;
+
+    float _head_turn_speed;
+
+public:
+
+    static void _register_methods();
+
+    Tank();
+    ~Tank();
+
+    void _ready();
+    void _init();
+
+    void _point_head_towards();
+
 
 };
 
